4878b064c94f78109190587e417affb84a7dce2c
Igor <i.kuzminskyy@thenewmotion.com>
2012-02-23 14:01:23 +0200

    making it possible to test without any db preconfig necessary (with the help of h2)

--- src/main/scala/com/transacttest/rest/TestRest.scala
+++ src/main/scala/com/transacttest/rest/TestRest.scala
@@ -1,10 +1,9 @@
 package com.transacttest.rest
 
-import net.liftweb.http.OkResponse
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.common.Loggable
 import com.transacttest.model.{Contact, User}
-import net.liftweb.db.{DefaultConnectionIdentifier, DB}
+import net.liftweb.http.{PlainTextResponse, OkResponse}
 
 /**
  */
@@ -12,13 +11,13 @@ import net.liftweb.db.{DefaultConnectionIdentifier, DB}
 object TestRest extends RestHelper with Loggable {
 
   serve {
-    "test" :: "transaction" :: Nil prefix {
-      case Get(_, _) =>
-        User.create.name("user").save()
-        interruptExecution()
-        Contact.create.name("contact").save()
-        OkResponse()
-    }
+    case Get("test" :: "transaction" :: Nil, _) =>
+      User.create.name("user").save()
+      interruptExecution()
+      Contact.create.name("contact").save()
+      OkResponse()
+    case Get("test" :: "get" :: Nil, _) =>
+      PlainTextResponse("Created. Users: %s, Contacts: %s".format(User.count, Contact.count))
   }
 
   def interruptExecution() {
